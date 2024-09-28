f697fb13efce1aed46bb7547b5f3e457355b0569
syedmuhammadmoin <46587349+syedmuhammadmoin@users.noreply.github.com>
2024-09-28 20:52:24 +0500

    stop container file updated

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get the container IDs of all running containers
+containerID=$(docker ps -q)
+
+# Check if any containers are found
+if [ -n "$containerID" ]; then
+    echo "Removing the following containers:"
+    echo "$containerID"
+    # Remove all running containers
+    docker rm -f $containerID
+    echo "All running containers have been removed."
+else
+    echo "No running containers to remove."
+fi
